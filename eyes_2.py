fcdacfd6277fe9d9a4b0306ee9d21288d8ad605c
jamtriv <51247333+jamtriv@users.noreply.github.com>
2021-08-23 14:39:17 +0100

    adding comments

--- eyes_2.py
+++ eyes_2.py
@@ -3,31 +3,31 @@ import pygame
 import math
 
 
-def update():
-    pass
+def update():                                                                 #defining update
+    pass                                                                      #used to add above function for syntax
 
-def draw():
-    screen.fill((0, 0, 0))
+def draw():                                                                   #defining draw
+    screen.fill((0, 0, 0))                                                    #filling screen with black colour
 
-    def draw_eye(eye_x, eye_y):
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-
-
-        distance_x = mouse_x - eye_x
-        distance_y = mouse_y - eye_y
-
-        distance = min(math.sqrt(distance_x ** 2 + distance_y ** 2), 30)
-        angle = math.atan2(distance_y, distance_x)
-
-        pupil_x = eye_x + (math.cos(angle) * distance)
-        pupil_y = eye_y + (math.sin(angle) * distance)
-
-        screen.draw.filled_circle((eye_x, eye_y), 50, color=(255, 255, 255))
-        screen.draw.filled_circle((pupil_x, pupil_y), 15, color=(0, 0, 100))
+    def draw_eye(eye_x, eye_y):                                               #defining function to draw eyes
+        mouse_x, mouse_y = pygame.mouse.get_pos()                             #getting x and y from mouse position.
 
 
-    draw_eye(200, 200)
-    draw_eye(330, 200)
+        distance_x = mouse_x - eye_x                                          #math to figure out the mouse is in relation to the eye
+        distance_y = mouse_y - eye_y                                          #math to figure out the mouse is in relation to the eye
+
+        distance = min(math.sqrt(distance_x ** 2 + distance_y ** 2), 30)      #math to figure out the actual distance, and not mnoving the eye.
+        angle = math.atan2(distance_y, distance_x)                            #math to figure out the reserve tan of the x and y distance
+
+        pupil_x = eye_x + (math.cos(angle) * distance)                        #finding cos of angle and then mutiplying by the distance
+        pupil_y = eye_y + (math.sin(angle) * distance)                        #finding sin of angle and then mutiplying by the distance
+
+        screen.draw.filled_circle((eye_x, eye_y), 50, color=(255, 255, 255))  #drawing eye1
+        screen.draw.filled_circle((pupil_x, pupil_y), 15, color=(0, 0, 100))  #drawing eye2
+
+
+    draw_eye(200, 200)                                                        #ACTUALLY drawing the eye1, but calling the function to draw the eye
+    draw_eye(330, 200)                                                        #ACTUALLY drawing the eye2, but calling the function to draw the eye
 
 
 
