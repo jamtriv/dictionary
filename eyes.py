bd478e1a415de049c8ee4f73be9d05f5498e19a7
jamtriv <51247333+jamtriv@users.noreply.github.com>
2021-08-23 13:16:19 +0100

    testing eyes

---
+++ eyes.py
@@ -0,0 +1,9 @@
+import pgzrun
+
+def draw():
+    screen.fill((0, 0, 0))
+
+    screen.draw.filled_circle((200, 200), 50, color=(255, 255, 255))
+    screen.draw.filled_circle((200, 200), 15, color=(0, 0, 100))
+
+pgzrun.go()
